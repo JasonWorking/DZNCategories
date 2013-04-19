2399887fb5cb62ef13922f3c9df7941bbaa23350
Ignacio Romero <iromero@dzen.cl>
2013-04-19 17:58:13 -0400

    Added CLLocationCoordinateRect struct and methods for geo-location bounding boxes support.

--- CoreLocation/CLLocation+Coordinate.h
+++ CoreLocation/CLLocation+Coordinate.h
@@ -7,13 +7,63 @@
 //
 
 #import <CoreLocation/CoreLocation.h>
+#include <math.h>
+
+static const double kDegreesToRadians = M_PI / 180.0;
+static const double kRadiansToDegrees = 180.0 / M_PI;
 
 @interface CLLocation (Coordinate)
+
+typedef struct {
+	CLLocationCoordinate2D bottomLeft;
+	CLLocationCoordinate2D topRight;
+} CLLocationCoordinateRect;
+
+typedef enum CLLocationCoordinateRectPreset {
+    CLLocationCoordinateRectPresetEmpty = 0,
+    CLLocationCoordinateRectPresetWorld,
+    CLLocationCoordinateRectPresetSanFransisco,
+    CLLocationCoordinateRectPresetNewYork,
+    CLLocationCoordinateRectPresetSantiago
+} CLLocationCoordinateRectPreset;
+
 
 /*
  * Return true if 'coord' is empty (that is, if it has zero values for latitude and longitude),
  * Return false otherwise. A null coordinate is defined to be empty.
  */
 bool CLLocationCoordinateIsEmpty(CLLocationCoordinate2D coord);
+
+/*
+ *
+ */
+NSString *NSStringFromCLLocationCoordinate(CLLocationCoordinate2D coord);
+
+/*
+ * Return true if 'rect' is empty (that is, if it has zero values for topLeft and bottomRight coordinates),
+ * Return false otherwise. A null rect is defined to be empty.
+ */
+bool CLLocationCoordinateRectIsEmpty(CLLocationCoordinateRect rect);
+
+/*
+ *
+ */
+NSString *NSStringFromCLLocationCoordinateRect(CLLocationCoordinateRect rect);
+
+/*
+ *
+ */
+CLLocationCoordinateRect CLLocationCoordinateRectMake(CLLocationCoordinate2D topLeft, CLLocationCoordinate2D bottomRight);
+
+/*
+ *
+ */
+CLLocationCoordinateRect CLLocationCoordinateRectWithPreset(CLLocationCoordinateRectPreset preset);
+
+/*
+ *
+ */
+CLLocationCoordinateRect CLLocationCoordinateRectFromCenter(CLLocationCoordinate2D center, CLLocationDistance radius);
+
 
 @end
--- Foundation/NSString+Regex.m
+++ Foundation/NSString+Regex.m
@@ -14,8 +14,8 @@
 
 - (BOOL)isValidEmail
 {
-    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
-    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",emailRegex];
+    NSString *regex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",regex];
     
     return [emailTest evaluateWithObject:self];
 }
