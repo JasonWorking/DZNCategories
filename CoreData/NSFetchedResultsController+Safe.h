de05b648d8fabcc94f3d92efeb254b1329d69cfe
Ignacio Romero <iromero@dzen.cl>
2013-08-12 16:52:03 -0400

    Added CoreData NSFetchedResultsController+Safe class

---
+++ CoreData/NSFetchedResultsController+Safe.h
@@ -0,0 +1,16 @@
+//
+//  NSFetchedResultsController+Safe.h
+//
+//  Created by Ignacio Romero Zurbuchen on 6/28/13.
+//  Copyright (c) 2011 DZen Interaktiv.
+//  Licence: MIT-Licence
+//  http://opensource.org/licenses/MIT
+//
+
+#import <CoreData/CoreData.h>
+
+@interface NSFetchedResultsController (Safe)
+
+- (id)safeObjectAtIndexPath:(NSIndexPath *)indexPath;
+
+@end
---
+++ CoreData/NSFetchedResultsController+Safe.m
@@ -0,0 +1,31 @@
+//
+//  NSFetchedResultsController+Safe.m
+//
+//  Created by Ignacio Romero Zurbuchen on 6/28/13.
+//  Copyright (c) 2011 DZen Interaktiv.
+//  Licence: MIT-Licence
+//  http://opensource.org/licenses/MIT
+//
+
+#import "NSFetchedResultsController+Safe.h"
+
+@implementation NSFetchedResultsController (Safe)
+
+- (id)safeObjectAtIndexPath:(NSIndexPath *)indexPath
+{
+    if ([self.fetchedObjects count] > 0)
+    {
+        NSInteger limit = 0;
+        for (int i = 0; i < (indexPath.section+1); i++) {
+            for (int j = 0; j < (indexPath.row+1); j++) {
+                limit++;
+            }
+        }
+
+        if (limit < [self.fetchedObjects count]) return [self objectAtIndexPath:indexPath];
+        else return nil;
+    }
+    else return nil;
+}
+
+@end
